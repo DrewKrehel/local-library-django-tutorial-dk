5548d19b0668d55b63569cd5c629082aa87f667f
Drew Krehel <drewkrehel@gmail.com>
2026-09-16 01:10:19 +0000

    Study PermissionRequiredMixin

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -185,6 +185,7 @@ class BookInstanceDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = BookInstance
     success_url = reverse_lazy('bookinstances')
 
+# Librarian Functions
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
@@ -195,5 +196,17 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
         return (
             BookInstance.objects.filter(borrower=self.request.user)
             .filter(status__exact='o')
+            .order_by('due_back')
+        )
+
+class LoanedBooksAllListView(LoginRequiredMixin, PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(status__exact='o')
             .order_by('due_back')
         )
